3cf4aa026371195a3a05ee43af57de28d00c5b2f
System Administrator <root@MacBook-Pro-de-everisIT.local>
2017-08-21 11:37:19 +0200

    First commit

---
+++ src/main/java/gbb/mavenHelloWorld/App.java
@@ -0,0 +1,13 @@
+package gbb.mavenHelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Gonzalico!" );
+    }
+}
